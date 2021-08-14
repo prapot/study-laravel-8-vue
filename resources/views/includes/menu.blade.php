da35ff9ddd6726ccff54adac5aaf115ba3e461e3
prapot <61309615+prapot@users.noreply.github.com>
2021-08-14 20:46:31 +0700

    days 2

---
+++ resources/views/includes/menu.blade.php
@@ -0,0 +1,28 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+    <div class="container">
+        <a class="navbar-brand" href="#">Study</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" href="{{route('home')}}">หน้าหลัก</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{route('about')}}">เกี่ยวกับเรา</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{route('service')}}">บริการ</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{route('contact')}}">ติดต่อเรา</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{route('login')}}">เข้าสู่ระบบ</a>
+                </li>
+                
+            </ul>
+        </div>
+    </div>
+</nav>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -8,11 +9,22 @@ use Illuminate\Support\Facades\Route;
 |--------------------------------------------------------------------------
 |
 | Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
+| routes are loaded by the RouteserviceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+
+// Route::get('/category/{name}', function ($name) {
+//     return $id; 
+// })->where('name','[0-9]+');
+
+Route::get('/',[HomeController::class,'home'])->name('home');
+Route::get('/about',[HomeController::class,'about'])->name('about');
+Route::get('/service',[HomeController::class,'service'])->name('service');
+Route::get('/contact',[HomeController::class,'contact'])->name('contact');
+Route::get('/login',[HomeController::class,'login'])->name('login');
